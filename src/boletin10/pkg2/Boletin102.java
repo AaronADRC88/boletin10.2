351016d936f7d4788506583ab6ecdbe8b4551116
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2015-01-12 09:25:49 +0100

    with javadoc

--- src/boletin10/pkg2/Boletin102.java
+++ src/boletin10/pkg2/Boletin102.java
@@ -1,7 +1,16 @@
 
 package boletin10.pkg2;
 
+/**
+ *
+ * @author aferreiradominguez
+ */
 public class Boletin102 {
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
     Xogo obx=new Xogo();
     obx.numtryes();
